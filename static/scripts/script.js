a0c050e6c66312ba3f7d7a7fe6d26e91bf59a29a
Kirsty Furey <kirstyfurey.kf@gmail.com>
2021-10-22 23:26:41 +0100

    add content and css to play mode and reset modals add javascript to open and close modals

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,27 +1,63 @@
 // JavaScript Document
+
+
 //Modal writing code amended from https://www.w3schools.com/howto/howto_css_modals.asp
 // Get the modal
-var modal = document.getElementById("myModal");
+var htpModal = document.getElementById("htp-modal");
+var pmModal = document.getElementById("pm-modal");
+var resetModal = document.getElementById("reset-modal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+var htpBtn = document.getElementById("htp-btn");
+var pmBtn = document.getElementById("pm-btn");
+var resetBtn = document.getElementById("reset-btn");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var htpSpan = document.getElementById("htp-close");
+var pmSpan = document.getElementById("pm-close");
+var resetSpan = document.getElementById("reset-close");
 
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+htpBtn.onclick = function() {
+	htpModal.style.display = "block";
+}
+
+pmBtn.onclick = function() {
+	pmModal.style.display = "block";
+}
+
+resetBtn.onclick = function (){
+	resetModal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+htpSpan.onclick = function() {
+	htpModal.style.display = "none";
+}
+
+pmSpan.onclick = function() {
+	pmModal.style.display = "none";
+}
+
+resetSpan.onclick = function () {
+	resetModal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+	if (event.target != htpModal) {
+		htpModal.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+	if (event.target == pmModal) {
+		pmModal.style.display = "none";
+  }
+}
+
+window.onclick = function(event) {
+	if (event.target == resetModal) {
+		resetModal.style.display = "none";
   }
 }
